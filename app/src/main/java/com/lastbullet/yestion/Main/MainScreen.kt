85d3194633d1b328b9ecb9b33d1cf192adf5ca18
Contacts0523 <gksso25@gmail.com>
2024-08-13 15:23:01 +0900

    메인화면

--- app/src/main/java/com/lastbullet/yestion/Main/MainScreen.kt
+++ app/src/main/java/com/lastbullet/yestion/Main/MainScreen.kt
@@ -23,6 +23,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.MoreVert
@@ -30,33 +31,40 @@ import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import coil.compose.rememberImagePainter
-
+import coil.compose.rememberAsyncImagePainter
+import kotlinx.coroutines.launch
+import okhttp3.internal.notify
 
 class MainScreen : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MainScreenComponent()
-
         }
     }
 }
@@ -71,105 +79,155 @@ fun MainScreenComponent() {
         imageUri.value = uri
     }
 
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text("Yestion")
-                },
-                navigationIcon = {
-                    IconButton(onClick = { /* TODO: 메뉴 아이콘 클릭 시 동작 */ }) {
-                        Icon(Icons.Default.Menu, contentDescription = "Menu")
-                    }
-                },
-                actions = {
-                    IconButton(onClick = {
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet(modifier = Modifier.fillMaxWidth(0.5f)) {
+                Text("Yestion 목록", modifier = Modifier.padding(16.dp))
+                Spacer(modifier = Modifier.height(8.dp))
+                NavigationDrawerItem(
+                    icon = { Icon(Icons.Default.Edit, contentDescription = null) },
+                    label = { Text("프로필 바꾸기") },
+                    selected = false,
+                    onClick = {
                         launcher.launch("image/*") // 갤러리에서 이미지를 선택하는 인텐트를 실행
-                    }) {
-                        Icon(Icons.Default.AccountCircle, contentDescription = "Profile")
+                        scope.launch { drawerState.close() }
                     }
-                },
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = Color(0xFFEDE7F6)
                 )
-            )
-        },
-        floatingActionButton = {
-            FloatingActionButton(onClick = { /* TODO: 추가 버튼 클릭 시 동작 */ }) {
-                Icon(Icons.Default.Add, contentDescription = "Add")
-            }
-        },
-        bottomBar = {
-            BottomAppBar(
-                containerColor = Color(0xFFEDE7F6)
-            ) {
-                Spacer(Modifier.weight(1f))
-                IconButton(onClick = { /* TODO: 홈 버튼 클릭 시 동작 */ }) {
-                    Icon(Icons.Default.Home, contentDescription = "Home")
-                }
-                IconButton(onClick = { /* TODO: 검색 버튼 클릭 시 동작 */ }) {
-                    Icon(Icons.Default.Search, contentDescription = "Search")
-                }
-                Spacer(Modifier.weight(1f))
+                NavigationDrawerItem(
+                    label = { Text("채팅방") },
+                    selected = false,
+                    onClick = {
+                        // TODO: 채팅방 클릭 시 동작 추가
+                        scope.launch { drawerState.close() }
+                    }
+                )
+                NavigationDrawerItem(
+                    label = { Text("노트") },
+                    selected = false,
+                    onClick = {
+                        // TODO: 노트 클릭 시 동작 추가
+                        scope.launch { drawerState.close() }
+                    }
+                )
             }
         }
-    ) { padding ->
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(padding)
-                .padding(16.dp),
-            horizontalAlignment = Alignment.Start
-        ) {
-            Row {
-                Box(
-                    modifier = Modifier
-                        .size(80.dp)
-                        .clip(CircleShape)
-                        .background(Color(0xFFD1C4E9))
+    ) {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = {
+                        Text("Yestion")
+                    },
+                    navigationIcon = {
+                        IconButton(onClick = {
+                            scope.launch {
+                                drawerState.open() // 드로어 열기
+                            }
+                        }) {
+                            Icon(Icons.Default.Menu, contentDescription = "Menu")
+                        }
+                    },
+                    actions = {
+                        IconButton(onClick = {
+                            launcher.launch("image/*") // 갤러리에서 이미지를 선택하는 인텐트를 실행
+                        }) {
+                            Icon(Icons.Default.AccountCircle, contentDescription = "Profile")
+                        }
+                    },
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = Color(0xFFEDE7F6)
+                    )
+                )
+            },
+            floatingActionButton = {
+                FloatingActionButton(onClick = { /* TODO: 추가 버튼 클릭 시 동작 */ }) {
+                    Icon(Icons.Default.Add, contentDescription = "Add")
+                }
+            },
+            bottomBar = {
+                BottomAppBar(
+                    containerColor = Color(0xFFEDE7F6)
                 ) {
-                    imageUri.value?.let { uri ->
-                        Image(
-                            painter = rememberImagePainter(uri),
+                    Spacer(Modifier.weight(1f))
+                    IconButton(onClick = { /* TODO: 홈 버튼 클릭 시 동작 */ }) {
+                        Icon(Icons.Default.Home, contentDescription = "Home")
+                    }
+                    IconButton(onClick = { /* TODO: 검색 버튼 클릭 시 동작 */ }) {
+                        Icon(Icons.Default.Search, contentDescription = "Search")
+                    }
+                    Spacer(Modifier.weight(1f))
+                }
+            }
+        ) { padding ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(padding)
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.Start
+            ) {
+                Row {
+                    Box(
+                        modifier = Modifier
+                            .size(80.dp)
+                            .clip(CircleShape)
+                            .background(Color(0xFFD1C4E9))
+                    ) {
+                        imageUri.value?.let { uri ->
+                            Image(
+                                painter = rememberAsyncImagePainter(uri),
+                                contentDescription = null,
+                                modifier = Modifier
+                                    .size(80.dp)
+                                    .clip(CircleShape)
+                            )
+                        } ?: Image(
+                            painter = rememberAsyncImagePainter(R.drawable.ic_launcher_background),
                             contentDescription = null,
                             modifier = Modifier
                                 .size(80.dp)
                                 .clip(CircleShape)
                         )
-                    } ?: Image(
-                        painter = rememberImagePainter(R.drawable.ic_launcher_background),
-                        contentDescription = null,
+                    }
+                    Box(
                         modifier = Modifier
-                            .size(80.dp)
-                            .clip(CircleShape)
-                    )
+                            .height(80.dp)
+                            .fillMaxWidth()
+                    ) {
+                        Text(
+                            text = "안드로이드",
+                            fontSize = 30.sp,
+                            modifier = Modifier
+                                .align(Alignment.CenterStart)
+                                .padding(start = 20.dp)
+                        )
+                    }
                 }
-                Box(
-                    modifier = Modifier
-                        .height(80.dp)
-                        .fillMaxWidth()
-                ) {
-                    Text(
-                        text = "안드로이드",
-                        fontSize = 30.sp,
-                        modifier = Modifier
-                            .align(Alignment.CenterStart)
-                            .padding(start = 20.dp)
-                    )
-                }
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                // 가로 카드 섹션
+                FeatureCard(
+                    title = "노트",
+                    description = "",
+                    imageRes = R.drawable.ic_launcher_background // 적절한 이미지 리소스를 사용하세요
+                )
+                FeatureCard(
+                    title = "채팅방",
+                    description = "Subhead",
+                    imageRes = R.drawable.ic_launcher_background // 적절한 이미지 리소스를 사용하세요
+                )
             }
-
-            Spacer(modifier = Modifier.height(16.dp))
-
-            // 가로 카드 섹션
-            FeatureCard(title = "노트", description = "")
-            FeatureCard(title = "채팅방", description = "Subhead")
         }
     }
 }
 
 @Composable
-fun FeatureCard(title: String, description: String) {
+fun FeatureCard(title: String, description: String, imageRes: Int) {
     Card(
         shape = RoundedCornerShape(10.dp),
         colors = CardDefaults.cardColors(
@@ -185,7 +243,9 @@ fun FeatureCard(title: String, description: String) {
                 .fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Box(
+            Image(
+                painter = painterResource(id = imageRes),
+                contentDescription = null,
                 modifier = Modifier
                     .size(40.dp)
                     .clip(CircleShape)
@@ -223,9 +283,6 @@ fun FeatureCard(title: String, description: String) {
     }
 }
 
-
-
-
 @Preview(showBackground = true)
 @Composable
 fun PreviewMainScreen() {
@@ -235,5 +292,9 @@ fun PreviewMainScreen() {
 @Preview(showBackground = true)
 @Composable
 fun PreviewFeatureCard() {
-    FeatureCard(title = "노트", description = "This is a subhead")
+    FeatureCard(
+        title = "노트",
+        description = "This is a subhead",
+        imageRes = R.drawable.ic_launcher_background // 적절한 이미지 리소스를 사용하세요
+    )
 }
